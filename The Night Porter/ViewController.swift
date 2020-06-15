a568a0b20bd5340ec4791547aa3de746ca97533c
daviidbartlett <daviidbartlett@gmail.com>
2020-06-15 21:21:43 +0100

    add table view with reusable cells

--- The Night Porter/ViewController.swift
+++ The Night Porter/ViewController.swift
@@ -8,8 +8,64 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    //Table View Delegate Methods
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        <#code#>
+    }
+    
+    
+    
+     //Table View Data Source Methods
+    let dailyTasks = ["eat","sleep","code"]
+    let weeklyTasks = ["vacuum","clean bathroom"]
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 2
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        switch section {
+        case 0:
+            return "Daily Tasks"
+        case 1:
+            return "Weekly Tasks"
+        default:
+            return "UH OH"
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        switch section {
+        case 0:
+           return dailyTasks.count
+        case 1:
+           return weeklyTasks.count
+        default:
+            return 0
+        }
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "normalCell", for: indexPath)
+        switch indexPath.section {
+        case 0:
+            cell.textLabel?.text = dailyTasks[indexPath.row]
+        case 1:
+            cell.textLabel?.text = weeklyTasks[indexPath.row]
+        default:
+            cell.textLabel?.text = "we shouldn't hit this!"
+        }
+        return cell
+    }
+    
 
+    
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
